b35ffcd1da10c37dda585c479e0d84779cc70469
白墨 <dasoncheng@foxmail.com>
2025-04-07 15:58:14 +0800

    init

---
+++ apps/passport/src/app.tsx
@@ -0,0 +1,11 @@
+import { defineComponent } from "vue";
+
+export const App = defineComponent({
+  setup() {
+    return () => (
+      <div class="h-screen w-screen flex items-center justify-center bg-gray-100">
+        <div class="text-3xl text-blue-500">Hello, Vite + Vue 3 + UnoCSS!</div>
+      </div>
+    );
+  },
+});
