8bd516da17da8636f8cc13484823104d59a47b95
dasoncheng <dasoncheng@outlook.com>
2025-04-08 16:37:58 +0800

    feat(passport): 添加shadcn

--- apps/passport/src/app.tsx
+++ apps/passport/src/app.tsx
@@ -4,7 +4,7 @@ export const App = defineComponent({
   setup() {
     return () => (
       <div class="h-screen w-screen flex items-center justify-center bg-gray-100">
-        <div class="text-3xl text-blue-500">Hello, Vite + Vue 3 + UnoCSS!</div>
+        <div class="text-3xl text-red-500">Hello, Vite + Vue 3 + UnoCSS!</div>
       </div>
     );
   },
--- apps/passport/vite.config.ts
+++ apps/passport/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
