b880e31a024474f0bfb671aea9fe582aba85cd8d
shengdong.cheng <shengdong.cheng@n2nconnect.com>
2025-04-14 12:59:11 +0800

    feat(lottery): 登录注册功能开发

--- apps/lottery/src/utils/http.ts
+++ apps/lottery/src/utils/http.ts
@@ -1,5 +1,13 @@
 import axios, { RawAxiosRequestHeaders } from "axios";
 import { mergeWith } from "lodash-es";
+import { useAuthStore } from "../stores/auth";
+
+const getAccount = () => {
+  const account = localStorage.getItem("account");
+  if (account) {
+    return JSON.parse(account);
+  }
+};
 
 // Common headers used across 996box API requests
 const commonHeaders: RawAxiosRequestHeaders = {
@@ -8,7 +16,11 @@ const commonHeaders: RawAxiosRequestHeaders = {
     "device:pc;channel:;appv:4.4.1.9007;deviceOs:win32 10.0.22631 32bit;deviceId:f4:f1:9e:2a:c4:6b;deviceBrand:;deviceModel:;netType:;gameOwner:;isSimulator:false;",
 };
 
-const noTokenUrlPatterns = ["api/v3/Room/detail"];
+const noTokenUrlPatterns = [
+  "api/v3/Room/detail",
+  "api/users",
+  "api/auth/login",
+];
 
 const http = axios.create({
   headers: commonHeaders,
@@ -16,19 +28,36 @@ const http = axios.create({
 
 http.interceptors.request.use((config) => {
   config.headers = mergeWith(commonHeaders, config.headers);
-
   if (
     !noTokenUrlPatterns.some((pattern) => {
       return config.url?.includes(pattern);
     })
   ) {
-    config.headers.tokem =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl92ZXJzaW9uIjoiVjEiLCJzdWIiOjEyMTcxNTU5NTcsImRldmljZV9pZCI6ImY0OmYxOjllOjJhOmM0OjZiIiwiYXBwX3ZlcnNpb24iOiI0LjQuMS45MDA3IiwibG9naW5fdGltZSI6MTc0MjQ1NDg5MSwiaXNzIjoiOTk2Ym94IiwiZXhwIjoxNzQ1MDQ2ODkxLCJkZXZpY2UiOiJwYyIsImlhdCI6MTc0MjQ1NDg5MSwianRpIjoiSXZnSXZ6Y1FKdkNyQktYMiJ9.amGS5w27zOx3KwDrQbZlatgWrAkQbn9y6znEZ1GOh8I";
+    const account = getAccount();
+    if (account.token) {
+      config.headers.Authorization = `Bearer ${account.token}`;
+    }
+    // config.headers.token =
+    //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl92ZXJzaW9uIjoiVjEiLCJzdWIiOjEyMTcxNTU5NTcsImRldmljZV9pZCI6ImY0OmYxOjllOjJhOmM0OjZiIiwiYXBwX3ZlcnNpb24iOiI0LjQuMS45MDA3IiwibG9naW5fdGltZSI6MTc0MjQ1NDg5MSwiaXNzIjoiOTk2Ym94IiwiZXhwIjoxNzQ1MDQ2ODkxLCJkZXZpY2UiOiJwYyIsImlhdCI6MTc0MjQ1NDg5MSwianRpIjoiSXZnSXZ6Y1FKdkNyQktYMiJ9.amGS5w27zOx3KwDrQbZlatgWrAkQbn9y6znEZ1GOh8I";
   }
   return config;
 });
-http.interceptors.response.use((response) => {
-  return response.data;
-});
+
+http.interceptors.response.use(
+  (response) => {
+    switch (response.status) {
+      case 401:
+        const authStore = useAuthStore();
+        authStore.logout();
+        authStore.openLoginDialog();
+        break;
+      default:
+        return response.data;
+    }
+  },
+  (error) => {
+    return Promise.reject(error.response?.data || error);
+  }
+);
 
 export { http };
