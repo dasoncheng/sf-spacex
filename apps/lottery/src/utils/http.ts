41e1ac382e167909706a78f6183c1e644839fe8c
shengdong.cheng <shengdong.cheng@n2nconnect.com>
2025-04-14 17:31:19 +0800

    fix(lottery): 修复请求拦截器中获取账户信息的逻辑并添加Authorization头

--- apps/lottery/src/utils/http.ts
+++ apps/lottery/src/utils/http.ts
@@ -1,6 +1,13 @@
 import axios, { RawAxiosRequestHeaders } from "axios";
 import { mergeWith } from "lodash-es";
 
+const getAccount = () => {
+  const account = localStorage.getItem("account");
+  if (account) {
+    return JSON.parse(account);
+  }
+};
+
 // Common headers used across 996box API requests
 const commonHeaders: RawAxiosRequestHeaders = {
   appv: "4.4.1.9007",
@@ -10,7 +17,7 @@ const commonHeaders: RawAxiosRequestHeaders = {
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl92ZXJzaW9uIjoiVjEiLCJzdWIiOjEyMTcxNTU5NTcsImRldmljZV9pZCI6ImY0OmYxOjllOjJhOmM0OjZiIiwiYXBwX3ZlcnNpb24iOiI0LjQuMS45MDA3IiwibG9naW5fdGltZSI6MTc0MjQ1NDg5MSwiaXNzIjoiOTk2Ym94IiwiZXhwIjoxNzQ1MDQ2ODkxLCJkZXZpY2UiOiJwYyIsImlhdCI6MTc0MjQ1NDg5MSwianRpIjoiSXZnSXZ6Y1FKdkNyQktYMiJ9.amGS5w27zOx3KwDrQbZlatgWrAkQbn9y6znEZ1GOh8I",
 };
 
-const noTokenUrlPatterns = ["api/v3/Room/detail"];
+const noTokenUrlPatterns = ["api/v3/Room/detail", "/auth/login", "/users"];
 
 const http = axios.create();
 
@@ -19,12 +26,14 @@ http.interceptors.request.use((config) => {
     config.headers = mergeWith(commonHeaders, config.headers);
   }
 
+  const account = getAccount();
   if (
     !noTokenUrlPatterns.some((pattern) => {
       return config.url?.includes(pattern);
-    })
+    }) &&
+    account.token
   ) {
-    // config.headers.Authorization = `Bearer ${account.token}`;
+    config.headers.Authorization = `Bearer ${account.token}`;
   }
   return config;
 });
