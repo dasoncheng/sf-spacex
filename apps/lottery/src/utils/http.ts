89da3a40eef83129df9c949a93280c5824146da6
dasoncheng <dasoncheng@outlook.com>
2025-04-14 17:24:41 +0800

    fix(lottery[http]): add 'auth/login' to noTokenUrlPatterns for improved request handling

--- apps/lottery/src/utils/http.ts
+++ apps/lottery/src/utils/http.ts
@@ -10,7 +10,7 @@ const commonHeaders: RawAxiosRequestHeaders = {
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl92ZXJzaW9uIjoiVjEiLCJzdWIiOjEyMTcxNTU5NTcsImRldmljZV9pZCI6ImY0OmYxOjllOjJhOmM0OjZiIiwiYXBwX3ZlcnNpb24iOiI0LjQuMS45MDA3IiwibG9naW5fdGltZSI6MTc0MjQ1NDg5MSwiaXNzIjoiOTk2Ym94IiwiZXhwIjoxNzQ1MDQ2ODkxLCJkZXZpY2UiOiJwYyIsImlhdCI6MTc0MjQ1NDg5MSwianRpIjoiSXZnSXZ6Y1FKdkNyQktYMiJ9.amGS5w27zOx3KwDrQbZlatgWrAkQbn9y6znEZ1GOh8I",
 };
 
-const noTokenUrlPatterns = ["api/v3/Room/detail"];
+const noTokenUrlPatterns = ["api/v3/Room/detail", "auth/login"];
 
 const http = axios.create();
 
