f0cc6cc41803f6d684ed86f7e31f72acfd287fca
shengdong.cheng <shengdong.cheng@n2nconnect.com>
2025-04-15 17:47:53 +0800

    feat(passport): 卡密状态修复

--- apps/passport/src/pages/licenses/LicensesList.tsx
+++ apps/passport/src/pages/licenses/LicensesList.tsx
@@ -160,7 +160,7 @@ export const LicensesList = defineComponent({
                       <TableCell>
                         <span
                           class={
-                            license.IsUsed
+                            license.Status
                               ? "inline-flex items-center rounded-full bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-700 ring-1 ring-inset ring-yellow-600/20"
                               : "inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
                           }
