5614c502b31c1c3ddea778883aca49ab52f153ce
shengdong.cheng <shengdong.cheng@n2nconnect.com>
2025-04-15 15:20:29 +0800

    feat(lottery): 激活逻辑修复；配置文件更新

--- apps/lottery/src/utils/environment.ts
+++ apps/lottery/src/utils/environment.ts
@@ -3,6 +3,7 @@ import { mergeWith } from "lodash-es";
 const environment = {
   mode: "local",
   baseUrl: "https://gateway.40mir.com",
+  applicationId: "0cc19a75-0d0f-4692-95b6-f2b88db96da6",
   996: {
     appv: "3.6.0",
     device: "ios",
